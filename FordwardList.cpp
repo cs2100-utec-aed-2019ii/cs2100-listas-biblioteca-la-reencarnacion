3303f928a49f0fe3373a030dcebf9f31fe5b323d
Unknown <victor.ascuna@utec.edu.pe>
2019-08-28 18:36:13 -0500

    feat(ForwardList): Atributos y Metodos

--- FordwardList.cpp
+++ FordwardList.cpp
@@ -3,3 +3,23 @@
 //
 
 #include "FordwardList.h"
+
+ForwardList::ForwardList(void){}
+ForwardList::~ForwardList(void){}
+
+ForwardList::T& front(void){}
+ForwardList::T& back(void){}
+        
+ForwardList::void push_back(const T& element){}
+ForwardList::void push_front(const T& element){}
+
+ForwardList::Node<T>* pop_back(void){}
+ForwardList::Node<T>* pop_front(void){}
+ForwardList::T& operator[] (const int&){}
+        
+ForwardList::bool empty(void){}
+ForwardList::unsigned int size(void){}
+ForwardList::void clear(void){}
+ForwardList::ForwardList& sort(void){}
+ForwardList::ForwardList& reverse(void){}
+ForwardList::std::ostream& operator << (const ForwardList&, std::ostream&){}
