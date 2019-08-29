f79eaead87be179709cd195c4e54cc1766dded71
Unknown <victor.ascuna@utec.edu.pe>
2019-08-29 17:52:15 -0500

    fix(FordwardList)

--- FordwardList.cpp
+++ FordwardList.cpp
@@ -5,22 +5,35 @@
 #include "FordwardList.h"
 #include <iostream>
 
-ForwardList::ForwardList(void){}
-ForwardList::~ForwardList(void){}
+template <typename T>
+ForwardList<T>::ForwardList(void){}
+template <typename T>
+ForwardList<T>::~ForwardList(void){}
 
-ForwardList::T& front(void){}
-ForwardList::T& back(void){}
+template <typename T>
+T& ForwardList<T>::front(void){}
+template <typename T>
+T& ForwardList<T>::back(void){}
         
 ForwardList::void push_back(const T& element){}
 ForwardList::void push_front(const T& element){}
 
-ForwardList::Node<T>* pop_back(void){}
-ForwardList::Node<T>* pop_front(void){}
-ForwardList::T& operator[] (const int&){}
-        
-ForwardList::bool empty(void){}
-ForwardList::unsigned int size(void){}
-ForwardList::void clear(void){}
-ForwardList::ForwardList& sort(void){}
-ForwardList::ForwardList& reverse(void){}
-ForwardList::std::ostream& operator << (const ForwardList&, std::ostream&){}
+template <typename T>
+Node<T>* ForwardList<T>::pop_back(void){}
+template <typename T>
+Node<T>* ForwardList<T>::pop_front(void){}
+template <typename T>
+T& ForwardList<T>::operator[] (const int&){}
+
+template <typename T>
+bool ForwardList<T>::empty(void){}
+template <typename T>
+unsigned int ForwardList<T>::size(void){}
+template <typename T>
+void ForwardList<T>::clear(void){}
+template <typename T>
+ForwardList<T>& ForwardList<T>::sort(void){}
+template <typename T>
+ForwardList<T>& ForwardList<T>::reverse(void){}
+template <typename T>
+ostream& ForwardList<T>::std::operator << (const ForwardList<T>&, std::ostream&){}
