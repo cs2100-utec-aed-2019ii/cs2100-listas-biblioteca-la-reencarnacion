26da8565825c809d1ee060b2adfd3a7398ec625b
marlon <marlon.mejia@utec.edu.pe>
2019-08-29 17:49:02 -0500

    feat(functionsA)

--- FordwardList.cpp
+++ FordwardList.cpp
@@ -3,12 +3,21 @@
 //
 
 #include "FordwardList.h"
+#include <iostream>
 
 ForwardList::ForwardList(void){}
 ForwardList::~ForwardList(void){}
 
-ForwardList::T& front(void){}
-ForwardList::T& back(void){}
+ForwardList::T& front(void){
+    return head->value();
+}
+ForwardList::T& back(void){
+    Node *temp=nullptr;
+    temp=head;
+    while(temp!=nullptr)
+        temp=temp->next();
+    return temp->value;
+}
         
 ForwardList::void push_back(const T& element){}
 ForwardList::void push_front(const T& element){}
@@ -18,7 +27,15 @@ ForwardList::Node<T>* pop_front(void){}
 ForwardList::T& operator[] (const int&){}
         
 ForwardList::bool empty(void){}
-ForwardList::unsigned int size(void){}
+ForwardList::unsigned int size(void){
+    int value=0;
+    head->next();
+    while(head!=nullptr){
+        head=head->nex();
+        value++;
+    }
+    return value;
+}
 ForwardList::void clear(void){}
 ForwardList::ForwardList& sort(void){}
 ForwardList::ForwardList& reverse(void){}
