bcaa101aadf09517dedb5f7abf0d3feea4952ce8
Luis Cordero Pinela <luis.cordero@utec.edu.pe>
2019-08-29 16:34:35 -0500

    feat(general)
    se añade el codigo base

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+<<<<<<< ad18d8e285ebb1066290c630447a2fb94c504da6
 using namespace std;
 
 int main() {
 
+=======
+int main (int, char*[]){
+    return 1;
+>>>>>>> feat(general)
 }
