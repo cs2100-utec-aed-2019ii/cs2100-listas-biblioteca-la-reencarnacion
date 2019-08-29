6e7be1b94975ddc23923721fd52fc19a931f4ec3
Unknown <victor.ascuna@utec.edu.pe>
2019-08-29 18:12:41 -0500

    fix(general)

--- FordwardList.cpp
+++ FordwardList.cpp
@@ -10,9 +10,11 @@ ForwardList<T>::ForwardList(void){}
 template <typename T>
 ForwardList<T>::~ForwardList(void){}
 
+template <typename T>
 ForwardList::T& front(void){
     return head->value();
 }
+template <typename T>
 ForwardList::T& back(void){
     Node *temp=nullptr;
     temp=head;
@@ -20,7 +22,8 @@ ForwardList::T& back(void){
         temp=temp->next();
     return temp->value;
 }
-        
+
+template <typename T>   
 void ForwardList<T>::push_back(const T& element){
     Node *node = nullptr;
     Node *final = new Node(element);
@@ -36,12 +39,16 @@ void ForwardList<T>::push_front(const T& element){
     node->next() = head;
     head = node;
 }
-
+template <typename T>
 ForwardList::Node<T>* pop_back(void){}
+template <typename T>
 ForwardList::Node<T>* pop_front(void){}
+template <typename T>
 ForwardList::T& operator[] (const int&){}
         
+template <typename T>
 ForwardList::bool empty(void){}
+template <typename T>
 ForwardList::unsigned int size(void){
     int value=0;
     head->next();
@@ -51,7 +58,11 @@ ForwardList::unsigned int size(void){
     }
     return value;
 }
+template <typename T>
 ForwardList::void clear(void){}
+template <typename T>
 ForwardList::ForwardList& sort(void){}
+template <typename T>
 ForwardList::ForwardList& reverse(void){}
+template <typename T>
 ForwardList::std::ostream& operator << (const ForwardList&, std::ostream&){}
