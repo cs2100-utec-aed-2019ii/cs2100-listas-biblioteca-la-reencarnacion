01444892181bcac0c13a72f4b8581482a0ec16e4
Unknown <victor.ascuna@utec.edu.pe>
2019-09-04 14:33:55 -0500

    fix(general)

--- main.cpp
+++ main.cpp
@@ -1,11 +1,4 @@
 #include <iostream>
-<<<<<<< ad18d8e285ebb1066290c630447a2fb94c504da6
-using namespace std;
-
-int main() {
-
-=======
 int main (int, char*[]){
     return 1;
->>>>>>> feat(general)
 }
