969a77c5d1224c14ce832b85189c2ac5cfd33f67
Unknown <victor.ascuna@utec.edu.pe>
2019-09-04 23:09:23 -0500

    fix(general)

--- FordwardList.cpp
+++ FordwardList.cpp
@@ -1,57 +1,53 @@
-//
-// Created by VICTOR on 28/08/2019.
-//
-
-#include "FordwardList.h"
-#include <iostream>
-
+#include "List.cpp"
+#include "ForwardListNode.cpp"
 template <typename T>
-ForwardList<T>::ForwardList(void){}
-template <typename T>
-ForwardList<T>::~ForwardList(void){}
-
-ForwardList::T& front(void){
-    return head->value();
-}
-ForwardList::T& back(void){
-    Node *temp=nullptr;
-    temp=head;
-    while(temp!=nullptr)
-        temp=temp->next();
-    return temp->value;
-}
-        
-void ForwardList<T>::push_back(const T& element){
-    Node *node = nullptr;
-    Node *final = new Node(element);
-    node = head;
-    while(node != nullptr){
-        node = node->next();
+class ForwardList : public List<T> {
+protected:
+    Node<T> *head;
+public:
+    ForwardList(){}
+    ~ForwardList(){}
+    T& front(){
+        return head->value();
     }
-    node->next() = final;
-}
-template <typename T>
-void ForwardList<T>::push_front(const T& element){
-    Node *node = new Node(element);
-    node->next() = head;
-    head = node;
-}
-
-ForwardList::Node<T>* pop_back(void){}
-ForwardList::Node<T>* pop_front(void){}
-ForwardList::T& operator[] (const int&){}
-        
-ForwardList::bool empty(void){}
-ForwardList::unsigned int size(void){
-    int value=0;
-    head->next();
-    while(head!=nullptr){
-        head=head->nex();
-        value++;
+    T& back(){
+        Node<T> *temp=nullptr;
+        temp=head;
+        while(temp!=nullptr)
+            temp=temp->next();
+        return temp->value;
     }
-    return value;
-}
-ForwardList::void clear(void){}
-ForwardList::ForwardList& sort(void){}
-ForwardList::ForwardList& reverse(void){}
-ForwardList::std::ostream& operator << (const ForwardList&, std::ostream&){}
+
+    void push_back(const T& element){
+        Node<T> *node = nullptr;
+        Node<T> *final = new Node<T>(element);
+        node = head;
+        while(node != nullptr){
+            node = node->next();
+        }
+        node->next() = final;
+    }
+    void push_front(const T& element){
+        Node *node = new Node(element);
+        node->next() = head;
+        head = node;
+    }
+    Node<T>* pop_back(void);
+    Node<T>* pop_front(void);
+    T& operator[] (const int&);
+
+    bool empty();
+    unsigned int size(void){
+        int value=0;
+        head->next();
+        while(head!=nullptr){
+            head=head->nex();
+            value++;
+        }
+        return value;
+    }
+    void clear(void);
+    ForwardList& sort(void);
+    ForwardList& reverse(void);
+    std::ostream& operator << (const ForwardList&, std::ostream&);
+};
