20615993ec79fb3d36e8fcca56a06c471b66e322
Luis Cordero Pinela <luis.cordero@utec.edu.pe>
2019-09-10 13:44:30 -0500

    feat(CircularList)
    generado el efecto circular a la lista

--- CircularList.cpp
+++ CircularList.cpp
@@ -1,10 +1,13 @@
 #include "List.cpp"
+#include "ForwardListNode.cpp"
 
 template <typename T>
 class CircularList : public List<T>{
 protected:
     ForwardListNode<T> *head;
 public:
-    CircularList(){}
+    CircularList(){
+        back()->next() = head;
+    }
     ~CircularList(){}
 };
